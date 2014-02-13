e5d45b32316014a6fe06138b0765aedc5c0f719f
jlucio <miahal77@gmail.com>
2014-02-13 08:59:11 -0600

    transactions_controller new
    Having trouble with conflicts in the transaction controller, so I
    deleted it and created another.

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,97 @@
+class TransactionsController < ApplicationController
+  http_basic_authenticate_with name: '', password: ''
+  # http_basic_authenticate_with name: '', password: ''
+  # GET /transactions
+  # GET /transactions.json
+  def index
+    @ledger_date = view_context.ledger_date
+    Transaction.duplicate_recurring(@ledger_date)
+    @transactions = Transaction.where(ledger_month: @ledger_date).includes(:vendor, :category)
+    #@transactions = Transaction.find_all_by_ledger_month(@ledger_date)
+
+    #if request.xhr?
+    #  render partial: 'month', layout: false
+    #  return
+    #end
+    logger.debug "+++++++++++IN TRANSACTIONS INDEX -> #{@transactions.inspect}"
+    respond_to do |format|
+      format.html # index.html.erb
+      #format.json { render layout: false }
+      format.json { render json: @transactions }
+    end
+  end
+
+  # GET /transactions/1
+  # GET /transactions/1.json
+  def show
+    @transaction = Transaction.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @transaction }
+    end
+  end
+
+  # GET /transactions/new
+  # GET /transactions/new.json
+  def new
+    @transaction = Transaction.new
+    @transaction.build_transaction_vendor
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @transaction }
+    end
+  end
+
+  # GET /transactions/1/edit
+  def edit
+    @transaction = Transaction.find(params[:id])
+  end
+
+  # POST /transactions
+  # POST /transactions.json
+  def create
+    @transaction = Transaction.new(params[:transaction])
+
+    respond_to do |format|
+      if @transaction.save_all(params)
+        format.html { redirect_to @transaction, notice: 'Transaction was successfully created.' }
+        format.json { render json: @transaction, status: :created, location: @transaction }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @transaction.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /transactions/1
+  # PUT /transactions/1.json
+  def update
+    @transaction = Transaction.find(params[:id])
+
+    logger.debug("==========PARAMS ARE #{params.inspect}")
+
+    updated_value = @transaction.update_attrs(params)
+    if updated_value != 'unable to update'
+      logger.debug("############################RETURNING #{updated_value}")
+      render json: updated_value, layout: false
+    else
+      logger.debug('############################RETURNING UNABLE TO UPDATE')
+      render json: 'unable to update', status: :unprocessable_entity, layout: false
+    end
+  end
+
+  # DELETE /transactions/1
+  # DELETE /transactions/1.json
+  def destroy
+    @transaction = Transaction.find(params[:id], include: :vendor)
+    vendor_name = @transaction.vendor.name.blank? ? 'Transaction' : "#{@transaction.vendor.name} transaction"
+
+    @transaction.destroy_and_remove_recurring
+    respond_to do |format|
+      format.html { redirect_to transactions_url, notice: "#{vendor_name} was successfully deleted." }
+      # render json: @transaction, layout: false
+    end
+  end
+end
